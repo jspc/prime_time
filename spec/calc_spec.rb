50fc26807e4f5c2c23c370131c7de50fa97cc017
jspc <james@zero-internet.org.uk>
2013-10-30 07:31:18 +0000

    Start writing tests

---
+++ spec/calc_spec.rb
@@ -0,0 +1,40 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'calc'
+
+describe Calc do
+  subject(:calc) { Calc.new }
+
+  context 'when instantiated' do
+    it { should be_true }
+    it { should respond_to :is_prime? }
+    it { should respond_to :list_of_primes }
+  end
+
+  context 'when calling #is_prime?' do
+    [2,367,499,571].each do |v|
+      it "should return true for valid prime #{v}" do
+        expect( calc.is_prime?(v) ).to be_true
+      end
+    end
+
+    [1,4,2028,1024].each do |v|
+      it "should return false for non prime #{v}" do
+        expect( calc.is_prime?(v) ).to be_false
+      end
+    end
+    
+    context 'when calling #list_of_primes with no args' do
+      it "should contain 10 elements" do
+        expect( calc.list_of_primes).to have(10).items
+      end
+    end
+
+    context 'when calling #list_of_primes(20)' do
+      it "should contain 20 elements" do
+        expect( calc.list_of_primes(20) ).to have(20).items
+      end
+    end
+
+  end
+end
